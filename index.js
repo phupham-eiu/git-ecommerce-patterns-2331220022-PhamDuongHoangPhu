6a270e002c83b1bda27ab84af7b41570fe4d76da
Phu Pham <phu.phamduong.cit23@eiu.edu.vn>
2026-05-07 08:13:52 +0700

    Part 2A: Singleton Pattern

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ console.log("--- E-Commerce Design Patterns Simulation ---");
 console.log("===========================================\n");
 
 // --- Part 2A: Singleton Pattern ---
-// console.log("--- 2A: Singleton Pattern ---");
-// const cart1 = new CartService();
-// const cart2 = new CartService();
-// cart1.addProduct({ id: 1, name: 'Laptop', price: 1200 });
-// console.log("Cart 1 contents:", cart1.getProducts());
-// console.log("Cart 2 contents:", cart2.getProducts());
-// console.log("Are cart1 and cart2 the same instance?", cart1 === cart2);
-// console.log("\n");
+console.log("--- 2A: Singleton Pattern ---");
+const cart1 = new CartService();
+const cart2 = new CartService();
+cart1.addProduct({ id: 1, name: 'Laptop', price: 1200 });
+console.log("Cart 1 contents:", cart1.getProducts());
+console.log("Cart 2 contents:", cart2.getProducts());
+console.log("Are cart1 and cart2 the same instance?", cart1 === cart2);
+console.log("\n");
 
 
 // --- Part 2B: Factory Pattern ---
