612f0b6f2d26c8b4ee4daa6d6542d258daf51e22
Phu Pham <phu.phamduong.cit23@eiu.edu.vn>
2026-05-07 08:23:40 +0700

    Part 2B: Factory Pattern

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ console.log("\n");
 
 
 // --- Part 2B: Factory Pattern ---
-// console.log("--- 2B: Factory Pattern ---");
-// const factory = new ProductFactory();
-// const book = factory.createProduct('book', { title: 'The Pragmatic Programmer', price: 35 });
-// const laptop = factory.createProduct('electronic', { model: 'XPS 15', price: 1500 });
-// book.describe();
-// laptop.describe();
-// console.log("\n");
+console.log("--- 2B: Factory Pattern ---");
+const factory = new ProductFactory();
+const book = factory.createProduct('book', { title: 'The Pragmatic Programmer', price: 35 });
+const laptop = factory.createProduct('electronic', { model: 'XPS 15', price: 1500 });
+book.describe();
+laptop.describe();
+console.log("\n");
 
 
 // --- Part 3A: Decorator Pattern ---
